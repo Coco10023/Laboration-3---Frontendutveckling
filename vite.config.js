a367a829e726a75b2c297287dc0b8762eaf081ad
Coco10023 <mauricio2003@live.se>
2026-02-12 15:04:52 +0100

    Hade missat att sass.html inte var på dist men fixade det nu

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,14 @@
 import { defineConfig } from "vite";
+import { resolve } from "path";
 
 export default defineConfig({
   base: "/Laboration-3---Frontendutveckling/",
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        sass: resolve(__dirname, "sass.html"),
+      },
+    },
+  },
 });
