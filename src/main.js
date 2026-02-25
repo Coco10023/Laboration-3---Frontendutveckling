14804ef2fe04eb1e767415fe3d5f0ed6f49bf8d0
Coco10023 <mauricio2003@live.se>
2026-02-25 23:50:33 +0100

    fixade så att kartan äntligen fungerar inom preview

--- src/main.js
+++ src/main.js
@@ -1,38 +1,10 @@
 import "./scss/main.scss";
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
 
 const btn = document.querySelector(".js-pulse");
-
 if (btn) {
   btn.addEventListener("click", () => {
     btn.classList.remove("is-pulsing");
-    // reflow trick för att kunna trigga om animationen
-    void btn.offsetWidth;
+    void btn.offsetWidth; // reflow för att trigga animation igen
     btn.classList.add("is-pulsing");
   });
 }
-
-
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
-
-setupCounter(document.querySelector('#counter'))
-
-
